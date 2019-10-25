6031b3e189baeab200bcfa547e034e197f4d1492
Paul D. Duncan <chesiredarling@gmail.com>
2019-10-24 20:49:42 -0700

    updated firebase & Stripe

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,14 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+const firebaseConfig = {
+  apiKey: "AIzaSyCR6YymPBobqL5mu1MUpS95RVssAEMp-e8",
+  authDomain: "pauz-db.firebaseapp.com",
+  databaseURL: "https://pauz-db.firebaseio.com",
+  projectId: "pauz-db",
+  storageBucket: "pauz-db.appspot.com",
+  messagingSenderId: "246061100968",
+  appId: "1:246061100968:web:4561eaf2cf69868c3bb79d",
+  measurementId: "G-CYCNBDWNGN"
 };
 
 firebase.initializeApp(config);
